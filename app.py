b5d2e02f9f1cc42af37bb8ebd7a2e1b3682a51e1
Stray Cat <130251613+KucingTetangga@users.noreply.github.com>
2023-11-07 21:16:25 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -3,15 +3,14 @@ from flask import request, redirect, url_for
 
 app = Flask(__name__)
 
-#open connection
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def halaman_awal():
 	return render_template('web.html')
 
-@app.route('/profile')
+@app.route('/profile/', methods=['GET'])
 def profile():
 	return render_template('profile.html')
 
 if __name__ == '__main__':
-	app.run(debug=True, port=8000)
+	app.run(debug=True, port=8000, host='0.0.0.0')
